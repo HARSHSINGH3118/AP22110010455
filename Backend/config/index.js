0aae85633ff850804c56b2be4d7592ede7dbad3e
Harsh Singh <harsh_singh@srmap.edu.in>
2025-03-20 19:57:31 +0530

    backend test 1

--- Backend/config/index.js
+++
@@ -1,7 +0,0 @@
-// config/index.js
-require("dotenv").config(); // Loads .env file into process.env
-
-module.exports = {
-  PORT: process.env.PORT || 3000,
-  BASE_URL: process.env.BASE_URL || "http://20.244.56.144/test",
-};
